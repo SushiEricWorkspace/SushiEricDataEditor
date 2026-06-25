3429fa47d3eddece550979f9a7135e16a515a930
toumokorosi01 <toumokorosi2289@gmail.com>
2026-06-25 19:43:29 +0900

    feat: DynamicStatsTypeにdefaultを追加

--- Common/src/main/kotlin/io/github/toumokorosi01/common/stats/DynamicStatsType.kt
+++ Common/src/main/kotlin/io/github/toumokorosi01/common/stats/DynamicStatsType.kt
@@ -7,9 +7,10 @@ enum class DynamicStatsType(
     val display: String,
     val color: TextColor,
     val min: Double,
-    val max: Double
+    val max: Double,
+    val default: Double
 ) {
-    HEALTH("HP", NamedTextColor.RED, 0.0, Double.MAX_VALUE),
-    MANA("マナ", NamedTextColor.AQUA, 0.0, Double.MAX_VALUE),
-    OVER_HEAL("オーバーヒール", NamedTextColor.YELLOW, 0.0, Double.MAX_VALUE)
+    HEALTH("HP", NamedTextColor.RED, 0.0, Double.MAX_VALUE, 100.0),
+    MANA("マナ", NamedTextColor.AQUA, 0.0, Double.MAX_VALUE, 100.0),
+    OVER_HEAL("オーバーヒール", NamedTextColor.YELLOW, 0.0, Double.MAX_VALUE, 0.0)
 }
