ecc797063330172b74d0c1b22e7e68f155234869
toumokorosi01 <toumokorosi2289@gmail.com>
2026-07-05 23:25:53 +0900

    fix: MobData保存時のcompleted更新を追加
    - MobManagerのsave処理をオーバーライド
    - 保存前にItemManagerからItemData一覧を読み込んでMobDataの検証に使用
    - validate結果をもとにrefreshCompletedを実行してcompleted状態を更新

--- Common/src/main/kotlin/io/github/toumokorosi01/common/data/mob/MobManager.kt
+++ Common/src/main/kotlin/io/github/toumokorosi01/common/data/mob/MobManager.kt
@@ -1,11 +1,23 @@
 package io.github.toumokorosi01.common.data.mob
 
+import io.github.toumokorosi01.common.Dir
 import io.github.toumokorosi01.common.data.core.ConfigurateDataManager
+import io.github.toumokorosi01.common.data.item.ItemManager
 import io.github.toumokorosi01.common.data.mob.data.MobData
+import java.io.File
 
 /**
  * [io.github.toumokorosi01.common.data.mob.data.MobData] のYAML保存・読み込みを担当するManager。
  */
 object MobManager : ConfigurateDataManager<MobData>(
     dataClass = MobData::class.java
-)
+) {
+    override fun save(file: File, data: MobData) {
+        val itemToSave = data.copy().apply {
+            val items = ItemManager.loadAll(Dir.Item.Stats.resolve(File(Dir.BASE_ROOT)))
+            refreshCompleted(validate(items))
+        }
+
+        super.save(file, itemToSave)
+    }
+}
