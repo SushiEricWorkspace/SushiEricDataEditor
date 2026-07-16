1db9ef711a3be0b745109fdeef87b17040ffe7e9
toumokorosi01 <toumokorosi2289@gmail.com>
2026-07-16 10:00:22 +0900

    feat: EntityStatsTypeにCRIT_RESISTANCEを追加

--- Common/src/main/kotlin/io/github/toumokorosi01/common/stats/entity/EntityStatsType.kt
+++ Common/src/main/kotlin/io/github/toumokorosi01/common/stats/entity/EntityStatsType.kt
@@ -7,5 +7,6 @@ enum class EntityStatsType(val display: String, val color: TextColor, val min: D
     HEALTH("HP", NamedTextColor.RED, 0.0, Double.MAX_VALUE, 100.0, true),
     MAX_HEALTH("最大体力", NamedTextColor.RED, 0.0, Double.MAX_VALUE, 100.0),
     ATTACK_DAMAGE("攻撃力", NamedTextColor.RED, 0.0, Double.MAX_VALUE, 2.0),
-    DEFENCE("防御力", NamedTextColor.GREEN, -Double.MAX_VALUE, Double.MAX_VALUE, 0.0)
+    DEFENCE("防御力", NamedTextColor.GREEN, -Double.MAX_VALUE, Double.MAX_VALUE, 0.0),
+    CRIT_RESISTANCE("会心抵抗率", NamedTextColor.AQUA, 0.0, Double.MAX_VALUE, 0.0)
 }
