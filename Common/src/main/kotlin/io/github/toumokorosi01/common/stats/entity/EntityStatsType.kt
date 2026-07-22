c7ce9d9150ba2ad1521bb3fe18d726691a024ce2
toumokorosi01 <toumokorosi2289@gmail.com>
2026-07-22 11:29:26 +0900

    refactor: EntityStatsからHEALTHを削除

--- Common/src/main/kotlin/io/github/toumokorosi01/common/stats/entity/EntityStatsType.kt
+++ Common/src/main/kotlin/io/github/toumokorosi01/common/stats/entity/EntityStatsType.kt
@@ -3,8 +3,7 @@ package io.github.toumokorosi01.common.stats.entity
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.format.TextColor
 
-enum class EntityStatsType(val display: String, val color: TextColor, val min: Double, val max: Double, val default: Double, val dynamic: Boolean = false) {
-    HEALTH("HP", NamedTextColor.RED, 0.0, Double.MAX_VALUE, 100.0, true),
+enum class EntityStatsType(val display: String, val color: TextColor, val min: Double, val max: Double, val default: Double) {
     MAX_HEALTH("最大体力", NamedTextColor.RED, 0.0, Double.MAX_VALUE, 100.0),
     ATTACK_DAMAGE("攻撃力", NamedTextColor.RED, 0.0, Double.MAX_VALUE, 2.0),
     DEFENCE("防御力", NamedTextColor.GREEN, -Double.MAX_VALUE, Double.MAX_VALUE, 0.0),
