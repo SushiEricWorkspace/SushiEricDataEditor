7a034edcb63a52101784acc73eacd8968c41a4b3
toumokorosi01 <toumokorosi2289@gmail.com>
2026-07-25 01:24:00 +0900

    refactor: 移動速度を調整 - StatsType.SPEEDの下限値を-200に変更 - EntityStatsType.SPEEDを追加

--- Common/src/main/kotlin/io/github/toumokorosi01/common/stats/entity/EntityStatsType.kt
+++ Common/src/main/kotlin/io/github/toumokorosi01/common/stats/entity/EntityStatsType.kt
@@ -9,5 +9,6 @@ enum class EntityStatsType(val display: String, val color: TextColor, val min: D
     DEFENCE("防御力", NamedTextColor.GREEN, -Double.MAX_VALUE, Double.MAX_VALUE, 0.0),
     CRIT_RESISTANCE("会心抵抗率", NamedTextColor.AQUA, 0.0, Double.MAX_VALUE, 0.0),
     AILMENT_DAMAGE("状態異常ダメージ", NamedTextColor.LIGHT_PURPLE, 0.0, Double.MAX_VALUE, 0.0),
-    AILMENT_RESISTANCE("状態異常耐性", NamedTextColor.LIGHT_PURPLE, 0.0, Double.MAX_VALUE, 0.0)
+    AILMENT_RESISTANCE("状態異常耐性", NamedTextColor.LIGHT_PURPLE, 0.0, Double.MAX_VALUE, 0.0),
+    SPEED("移動速度", NamedTextColor.WHITE, -Double.MAX_VALUE, Double.MAX_VALUE, 100.0)
 }
